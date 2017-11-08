64ac70f120e8e5ce9212c5a2dece70c0ca523221
Vojtech Jirsak <fogs222@users.noreply.github.com>
2017-11-08 10:36:58 +0100

    DrawTask Finish method added

--- main/java/cz/cuni/pedf/vovap/jirsak/geostezka/TaskDrawActivity.java
+++ main/java/cz/cuni/pedf/vovap/jirsak/geostezka/TaskDrawActivity.java
@@ -2,8 +2,11 @@ package cz.cuni.pedf.vovap.jirsak.geostezka;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import cz.cuni.pedf.vovap.jirsak.geostezka.tasks.DrawTask;
 import cz.cuni.pedf.vovap.jirsak.geostezka.utils.BaseTaskActivity;
@@ -13,10 +16,11 @@ import cz.cuni.pedf.vovap.jirsak.geostezka.utils.InitDB;
 
 public class TaskDrawActivity extends BaseTaskActivity {
     private static final String LOG_TAG = "GEO TaskDrawActivity";
-    DrawTask dt;
+    public DrawTask dt;
     InitDB db;
     boolean finished;
     DrawTaskCanvas canvas;
+    public ImageView back;
 
 	//ImageView bckImage;
 
@@ -30,14 +34,19 @@ public class TaskDrawActivity extends BaseTaskActivity {
         int predaneID = mIntent.getIntExtra("id", 7);
         dt = (DrawTask) Config.vratUlohuPodleID(predaneID);
         super.init(dt.getNazev(), dt.getZadani());
-
 		setContentView(dt.getLayout());
-		LinearLayout bck = (LinearLayout) findViewById(R.id.dtBckImage);
+		RelativeLayout bck = (RelativeLayout) findViewById(R.id.dtBckImage);
 		bck.setBackgroundResource(dt.getBckFinal());
-
+        back = (ImageView)findViewById(R.id.confirmTask);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TaskDrawActivity.this,DashboardActivity.class));
+            }
+        });
 		canvas = (DrawTaskCanvas) findViewById(R.id.dtCanvas);
 		canvas.setBtmResourceId(dt.getBckStart());
-
+        canvas.setCtx(this);
 
 		db = new InitDB(this);
         db.open();
@@ -46,12 +55,25 @@ public class TaskDrawActivity extends BaseTaskActivity {
 			UkazZadani(dt.getNazev(), dt.getZadani());
 		}else if(db.vratStavUlohy(dt.getId()) == Config.TASK_STATUS_DONE) {
             finished = true;
+            //canvas.setVisibility(View.INVISIBLE);
+            canvas.finishTask();
         }
         db.close();
     }
 
+    public boolean isFinished(){return finished;}
+
     @Override
     public void runFromResultDialog(boolean result, boolean closeTask) {
+        if(result) {
+            /// bylo pouze zobrazeni spravne odpovedi
+            if(closeTask) {
+                startActivity(new Intent(TaskDrawActivity.this, DashboardActivity.class));
+                finish();
+            }
+        }else {
+            Log.d(LOG_TAG, "FAULT RESULT do nothing");
+        }
 
     }
 }
